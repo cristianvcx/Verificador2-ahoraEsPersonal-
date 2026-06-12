43fd9796569458a1ad6f77796ec2142114aa105c
Mateo <mateo.ossa.b@gmail.com>
2026-06-12 15:57:23 -0400

    feat(ux): implementar polling pasivo y boton de actualizacion manual para auditores
    - Agregar directiva `wire:poll.600s` en `consulta-list.blade.php` condicionada al rol de auditor
    - Crear botón institucional de actualización manual rápida mediante `$refresh`
    - Añadir estados visuales de carga durante el refresco asíncrono

--- resources/views/livewire/actividades/consulta-list.blade.php
+++ resources/views/livewire/actividades/consulta-list.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ advancedOpen: false }">
+<div x-data="{ advancedOpen: false }" @if(Auth::user()->rol === 'auditor') wire:poll.600s @endif>
     <!-- 1. Filtros Básicos y Avanzados -->
     @include('livewire.actividades.partials.filtros')
 
